c4e463216b7b1b13ae705333a37979450e309058
iiiRyan <85ryan.blue@gmail.com>
2023-12-24 23:06:09 +0800

    fix: fix bug

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -28,7 +28,7 @@ const MAP_LAYER_LIST = [
 ];
 
 const USE_GOOGLE_ANALYTICS = true;
-const GOOGLE_ANALYTICS_TRACKING_ID = 'GTM-WDN3LTB5';
+const GOOGLE_ANALYTICS_TRACKING_ID = 'G-JPRJ7E638G';
 
 // styling: set to `true` if you want dash-line route
 const USE_DASH_LINE = true;
